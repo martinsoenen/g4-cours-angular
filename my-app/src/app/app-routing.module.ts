3bb5f82c631e0cff756f335f1b303cb9df47fb36
Martin Soenen <martin.soenen@outlook.fr>
2021-02-11 17:14:54 +0100

    Begin the Angular test project

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { FormulaireComponent } from "./formulaire/formulaire.component";
+
+const routes: Routes = [
+  { path: 'formulaire', component: FormulaireComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
