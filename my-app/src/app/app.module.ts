7c25e6c251d26d490557d735f36b3f5364c43570
Vincent AMSTOUTZ <vincent.amstoutz@outlook.fr>
2021-02-11 14:19:53 +0100

    feature : add app-routing

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormulaireComponent } from './formulaire/formulaire.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, FormulaireComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
