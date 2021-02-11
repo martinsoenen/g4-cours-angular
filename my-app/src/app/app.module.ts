5e60da59ac6294db17f76a789cfdfa85bf5ce87b
Martin Soenen <martin.soenen@outlook.fr>
2021-02-11 15:25:35 +0100

    End of the first project

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormulaireComponent } from './formulaire/formulaire.component';
 import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { FormsModule } from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
