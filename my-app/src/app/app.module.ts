d8b89ac340b61877200f00a8df4db96730406575
AlexChapelle <alexchapelle971@gmail.com>
2021-02-11 10:39:49 +0100

    Init Angular project

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormulaireComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
